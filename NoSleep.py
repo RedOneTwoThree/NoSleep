7872757a7c9806fd399ea8ee26fc8d2d0fb52e6c
mkhan124 <mkhan124@jaguarlandrover.com>
2024-01-30 15:47:50 +0000

    made adjustments to the timer

--- NoSleep.py
+++ NoSleep.py
@@ -9,8 +9,8 @@ width, height = pyautogui.size()
 
 try:
     while True:
-        time.sleep(15)
-        for i in range(0, 100):
+        time.sleep(120)
+        for i in range(0, 50):
             pyautogui.moveTo(0, i * 5)
         for i in range(0, 3):
             pyautogui.press('shift')
